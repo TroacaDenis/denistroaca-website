25c07d5b43e94e9544ecdacc0c4ca1ec47c7b75d
TroacaDenis <denistroaca@yahoo.com>
2021-12-01 22:43:00 +0200

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>online shop</title>
+</head>
+<body>
+    <a href = "signup.php">Sign up</a>
+    <a href = "signup.php">Log in</a>
+    <a href = "signup.php">Log out</a>
+</body>
+</html>
